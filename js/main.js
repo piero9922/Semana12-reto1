1c495117de03093c971c4d06813a50c364188141
Piero Montalvo <piero.montalvo9922@gmail.com>
2020-06-20 10:36:46 -0500

    Primer reto, Semana12

--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
 const dropdown = document.getElementById('dropdown')
 
-window.addEventListener('load', () =>{    
-    
-    const option2 = document.createElement('option')
-    option2.textContent = 'Inicio'
+const option2 = document.createElement('option')
+option2.textContent = 'Inicio'
 
-    const option3 = document.createElement('option')
-    option3.textContent = 'Nosotros'
+const option3 = document.createElement('option')
+option3.textContent = 'Nosotros'
 
-    const option4 = document.createElement('option')
-    option4.textContent = 'Portafolio'
+const option4 = document.createElement('option')
+option4.textContent = 'Portafolio'
 
-    const option5 = document.createElement('option')
-    option5.textContent = 'Contactanos'
+const option5 = document.createElement('option')
+option5.textContent = 'Contactanos'
 
+dropdown.addEventListener('mouseover', () =>{    
     dropdown.appendChild(option2)
     dropdown.appendChild(option3)
     dropdown.appendChild(option4)
     dropdown.appendChild(option5)
+
+    dropdown.setAttribute('size', 5)
     
 })
 
 
-dropdown.addEventListener('mouseenter', ()=>{
-    
+dropdown.addEventListener('mouseout', ()=>{
+    dropdown.removeChild(option2)
+    dropdown.removeChild(option3)
+    dropdown.removeChild(option4)
+    dropdown.removeChild(option5)
+
+    dropdown.setAttribute('size', 1)
+
 })
 
 
